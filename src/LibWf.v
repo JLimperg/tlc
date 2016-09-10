6cf6228ceb0bf9777313f903bb40c3d976ef8515
Jannis Limperg <jannis@limperg.de>
2016-09-10 19:30:27 +0200

    LibWf: Proof of tclosure_wf
    This was previously an axiom.

--- src/LibWf.v
+++ src/LibWf.v
@@ -530,47 +530,42 @@ End WfUnion.
 (* ********************************************************************** *)
 (** * Transitive closure *)
 
-(* TODO
-
 Section Wf_Transitive_Closure.
-  Variable A : Type.
-  Variable R : relation A.
 
-  Notation trans_clos := (clos_trans A R).
+Lemma Acc_clos_trans : forall A (R:binary A) x,
+  Acc R x -> Acc (tclosure' R) x.
+Proof.
+  induction 1 as [x0 _ H1]. apply Acc_intro. introv H2.
+  induction H2; auto with sets.
+  apply Acc_inv with y; auto with sets.
+Qed.
 
-  Lemma incl_clos_trans : inclusion A R trans_clos.
-    red in |- *; auto with sets.
-  Qed.
+Hint Resolve Acc_clos_trans.
 
-  Lemma Acc_clos_trans : forall x:A, Acc R x -> Acc trans_clos x.
-    induction 1 as [x0 _ H1].
-    apply Acc_intro.
-    intros y H2.
-    induction H2; auto with sets.
-    apply Acc_inv with y; auto with sets.
-  Qed.
+Lemma Acc_inv_trans : forall A (R:binary A) x y,
+  tclosure' R y x -> Acc R x -> Acc R y.
+Proof using.
+  induction 1 as [| x y]; auto with sets.
+  intro; apply Acc_inv with y; assumption.
+Qed.
 
-  Hint Resolve Acc_clos_trans.
-
-  Lemma Acc_inv_trans : forall x y:A, trans_clos y x -> Acc R x -> Acc R y.
-  Proof using.
-    induction 1 as [| x y]; auto with sets.
-    intro; apply Acc_inv with y; assumption.
-  Qed.
-
-  Theorem wf_clos_trans : well_founded R -> well_founded trans_clos.
-  Proof using.
-    unfold well_founded in |- *; auto with sets.
-  Qed.
-
-End Wf_Transitive_Closure.
-
-*)
+Lemma tclosure'_wf : forall A (R:binary A),
+  wf R -> wf (tclosure' R).
+Proof using.
+  unfold wf; unfold well_founded; auto with sets.
+Qed.
 
 Lemma tclosure_wf : forall A (R:binary A),
   wf R -> wf (tclosure R).
 Proof using.
-Admitted. (* TODO: adapt proof from the standard library; see above *)
+  introv H. eapply wf_iff.
+  - apply tclosure'_wf; eassumption.
+  - symmetry. apply tclosure_tclosure'.
+Qed.
+
+End Wf_Transitive_Closure.
+
+Hint Resolve tclosure_wf tclosure'_wf : wf.
 
 (* end hide *)
 
